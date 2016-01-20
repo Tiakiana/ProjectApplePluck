27238cb453af295a573f83995acf101b70c9719f
RiderDoc <Emil.olesen@gmail.com>
2016-01-20 11:38:52 +0100

    Emil Sale class Work in progress

---
+++ ProjectOrchard/Sale.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectOrchard
+{
+    class Sale
+    {
+        private string Type { get; }
+        private decimal Kg { get; }
+        private decimal Price { get; }
+        private string Location { get; }
+        private DateTime Date { get; }
+
+        public Sale(string type, decimal kg, decimal price, string location, DateTime date)
+        {
+            Type = type;
+            Kg = kg;
+            Price = price;
+            Location = location;
+            Date = date;
+        }
+    }
+}
