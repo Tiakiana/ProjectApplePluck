2e5c200983b1f2b549dfa7bde82431970d9c12ad
Simon Glashoff <Simon Glashoff>
2016-01-20 11:27:29 +0100

    TeamPas added flere tests

--- ProfitTests/UnitTest1.cs
+++ ProfitTests/UnitTest1.cs
@@ -9,13 +9,45 @@ namespace ProfitTests
         [TestMethod]
         public void TestMethod1()
         {
-            ProfitCalc pc = new ProfitCalc;
+            ProfitCalc pc = new ProfitCalc();
             pc.income = 200;
             pc.expenses = 100;
 
             double profit = pc.CalcProfit(pc.income, pc.expenses);
 
             Assert.AreEqual(profit, 100);
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ProfitCalc pc = new ProfitCalc();
+            pc.income = 200;
+            pc.expenses = -100;
+
+            double profit = pc.CalcProfit(pc.income, pc.expenses);
+
+            Assert.AreEqual(profit, 300);
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ProfitCalc pc = new ProfitCalc();
+            pc.income = 200.6543;
+            pc.expenses = 100.234;
+
+            double profit = pc.CalcProfit(pc.income, pc.expenses);
+
+            Assert.AreEqual(profit, 100.42);
+        }
+        public void TestMethod1()
+        {
+            ProfitCalc pc = new ProfitCalc();
+            pc.income;
+            pc.expenses;
+
+            double profit = pc.CalcProfit(pc.income, pc.expenses);
+
+            Assert.AreEqual(profit, 0);
         }
     }
 }
