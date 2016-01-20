fb53f5f0eb289f98a768ae7f0dd64251ce2da2b7
Simon Glashoff <Simon Glashoff>
2016-01-20 10:24:58 +0100

    TeamPas added test project - - startet

---
+++ ProfitTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProfitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ProfitCalc pc = new ProfitCalc;
+            pc.income = 200;
+            pc.expenses = 100;
+
+            double profit = pc.CalcProfit(pc.income, pc.expenses);
+
+            Assert.AreEqual(profit, 100);
+        }
+    }
+}
